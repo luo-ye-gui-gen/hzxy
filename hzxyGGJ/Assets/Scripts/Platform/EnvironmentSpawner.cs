ab2eca1ea807a1fb9f7a58c1f3b38c2fe507f0ba
321blooming <2814465037@qq.com>
2026-01-20 22:22:31 +0800

    平台循环（待改）

--- hzxyGGJ/Assets/Scripts/Platform/EnvironmentSpawner.cs
+++ hzxyGGJ/Assets/Scripts/Platform/EnvironmentSpawner.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class EnvironmentSpawner : MonoBehaviour
 {
     [Tooltip("ÊäÈëÔ¤ÖÆÌå")]
-    public GameObject environmentPrefab;
+    public List<GameObject> environmentPrefab = new List<GameObject>();
     private bool hasSpawner = false;
+
+    public int i,k = 0;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -14,12 +16,13 @@ public class EnvironmentSpawner : MonoBehaviour
         if(!collision.CompareTag("Player")) return;
 
         var segment = GetComponentInParent<EnvironmentController>();
-        if (segment == null || segment.spawnPoint == null)
+        if (segment == null || segment.spawnPointsList == null)
         {
             return;
         }
-
-        Instantiate(environmentPrefab,segment.spawnPoint.position,segment.spawnPoint.rotation);
+        i = Random.Range(0, segment.spawnPointsList.Count);
+        k= i;
+        Instantiate(environmentPrefab[k], segment.spawnPointsList[i].position, segment.spawnPointsList[i].rotation);
 
         hasSpawner = true;
     }
